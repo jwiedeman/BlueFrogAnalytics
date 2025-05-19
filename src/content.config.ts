ab16c849256f7432116450b324295aa9ed60c310
Joshua Wiedeman <joshua.f.wiedeman@gmail.com>
2025-05-19 14:41:42 -0700

    Use page template for Analytics Doctor

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,14 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
 import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
 
+const extendedDocsSchema = docsSchema({
+  extend: (schema) =>
+    schema.extend({
+      template: z.enum(['doc', 'splash', 'page']).default('doc'),
+    }),
+});
+
 export const collections = {
-	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+  docs: defineCollection({ loader: docsLoader(), schema: extendedDocsSchema }),
 };
