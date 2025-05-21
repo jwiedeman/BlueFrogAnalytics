f7a3190edf72ebbf5cdc19b7a1adc2da0f7136c8
Joshua Wiedeman <joshua.f.wiedeman@gmail.com>
2025-05-20 18:57:48 -0700

    Fix legal page templates

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,7 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection } from 'astro:content';
 import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
 
-// Extend the default Starlight docs schema to allow the `page` template
-const extendedDocsSchema = docsSchema({
-  extend: z.object({
-    template: z.enum(['doc', 'splash', 'page']).default('doc'),
-  }),
-});
-
 export const collections = {
-  docs: defineCollection({ loader: docsLoader(), schema: extendedDocsSchema }),
+  docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
 };
