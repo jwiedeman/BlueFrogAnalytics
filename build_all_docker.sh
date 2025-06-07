fc48bac7974ae72a713270d26d54ff94a9d4e0a3
Joshua Wiedeman <joshua.f.wiedeman@gmail.com>
2025-06-07 14:11:10 -0700

    fix: build script pulls certstream-server before building

--- build_all_docker.sh
+++ build_all_docker.sh
@@ -11,12 +11,43 @@ find "$BOTS_DIR" -name 'Dockerfile*' | while read -r dockerfile; do
     | tr '[:upper:]' '[:lower:]' \
     | tr '/[] _' '--' \
     | sed -e 's/--*/-/g' -e 's/^-//' -e 's/-$//')
+
+  build_context="$dir"
+  tmp_cleanup=""
+
+  case "$dockerfile" in
+    */certstream-server/frontend/Dockerfile)
+      tmpdir=$(mktemp -d)
+      git clone --depth 1 https://github.com/CaliDog/certstream-server.git "$tmpdir" >/dev/null
+      build_context="$tmpdir/frontend"
+      tmp_cleanup="$tmpdir"
+      ;;
+    */certstream-server/Dockerfile)
+      tmpdir=$(mktemp -d)
+      git clone --depth 1 https://github.com/CaliDog/certstream-server.git "$tmpdir" >/dev/null
+      build_context="$tmpdir"
+      tmp_cleanup="$tmpdir"
+      ;;
+    */Dockerfile.combined)
+      tmpdir=$(mktemp -d)
+      cp -r "$dir"/* "$tmpdir"/
+      rm -rf "$tmpdir/certstream-server"
+      git clone --depth 1 https://github.com/CaliDog/certstream-server.git "$tmpdir/certstream-server" >/dev/null
+      build_context="$tmpdir"
+      tmp_cleanup="$tmpdir"
+      ;;
+  esac
+
   echo "\nBuilding $tag from $dockerfile"
-  if docker build -f "$dockerfile" -t "$tag" "$dir"; then
+  if docker build -f "$dockerfile" -t "$tag" "$build_context"; then
     echo "Built $tag"
   else
     echo "Failed to build $tag; continuing..."
   fi
-done
 
+  if [ -n "$tmp_cleanup" ]; then
+    rm -rf "$tmp_cleanup"
+  fi
+
+done
 
