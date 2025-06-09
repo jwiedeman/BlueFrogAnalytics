a668bb4875f593dc4387a93e422649969036a0dd
Joshua Wiedeman <joshua.f.wiedeman@gmail.com>
2025-06-09 16:47:47 -0700

    fix vite alias for shadcn

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,14 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import { fileURLToPath } from 'url';
 
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-      plugins: [tailwindcss()]
-	},
+      plugins: [tailwindcss()],
+      resolve: {
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+      },
+        },
 
   integrations: [react()]
 });
